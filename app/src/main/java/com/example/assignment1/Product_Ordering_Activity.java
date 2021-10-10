fe5eda0485d03f61dfcc0be5b079c46c396827f6
BrandonCMohammed <64518176+BrandonCMohammed@users.noreply.github.com>
2021-10-09 22:41:29 -0400

    Simple functionality was added to the add item button and the function for the complete order was started

--- app/src/main/java/com/example/assignment1/Product_Ordering_Activity.java
+++ app/src/main/java/com/example/assignment1/Product_Ordering_Activity.java
@@ -3,6 +3,8 @@ package com.example.assignment1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Product_Ordering_Activity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class Product_Ordering_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.product_ordering);
+    }
+
+    public void addItem(View view){
+        TextView textview = (TextView) findViewById(R.id.ProductListing);
+        textview.setText("Text");
+
+    }
+
+    public void completeOrder(View view){
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/assignment1/Product_Specification_Activiy.java
@@ -0,0 +1,14 @@
+package com.example.assignment1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Product_Specification_Activiy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_specification_activiy);
+    }
+}
