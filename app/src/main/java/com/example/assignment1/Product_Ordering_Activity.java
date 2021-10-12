a3cba5db10d12a6d53f5c0818a1d7f8459f28dd3
BrandonCMohammed <64518176+BrandonCMohammed@users.noreply.github.com>
2021-10-12 19:50:32 -0400

    Added intent to allow info to be sent between the activities

--- app/src/main/java/com/example/assignment1/Product_Ordering_Activity.java
+++ app/src/main/java/com/example/assignment1/Product_Ordering_Activity.java
@@ -3,6 +3,7 @@ package com.example.assignment1;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -14,24 +15,65 @@ import java.util.ArrayList;
 public class Product_Ordering_Activity extends AppCompatActivity {
 
     ArrayList<String> flavors = new ArrayList<String>();
+    public static final String specifications = "choice";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.product_ordering);
+
         ImageView imageview = (ImageView) findViewById(R.id.logo);
         imageview.setImageAlpha(100);
-    }
 
-    public void addItem(View view){
         TextView textview = (TextView) findViewById(R.id.ProductListing);
-        textview.setText("Welcome Casidy");
+
+        Intent intent = getIntent();
+        String totalSpecifications = intent.getStringExtra(specifications);
+        if ( totalSpecifications == ""){
+            textview.setText("Products selected will be shown here:");
+        }else{
+            textview.setText(totalSpecifications);
+        }
 
     }
+
+    public void onCheckboxClicked(View view){
+
+        CheckBox checkbox = (CheckBox)view;
+        boolean checked = checkbox.isChecked();
+        String product = "";
+
+        switch (view.getId()){
+
+            case R.id.Product1:
+                if(checked){
+                    product = checkbox.getText().toString();
+                }
+                break;
+            case R.id.Product2:
+                if(checked) {
+                    product = checkbox.getText().toString();
+                }
+
+            case R.id.Product3:
+                if(checked){
+                    product = checkbox.getText().toString();
+                }
+        }
+
+        Intent intent = new Intent(this, Product_Specification_Activity.class);
+
+        if(checked){
+            intent.putExtra(Product_Specification_Activity.productChoice, product);
+        }
+        startActivity(intent);
+
+    }
+
+
 
     public void completeOrder(View view){
-        Intent intent = new Intent(this, Product_Specification_Activity.class);
-        startActivity(intent);
+
 
 
     }
--- app/src/main/java/com/example/assignment1/Product_Specification_Activity.java
+++ app/src/main/java/com/example/assignment1/Product_Specification_Activity.java
@@ -6,15 +6,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class Product_Specification_Activity extends AppCompatActivity {
+
+    public static final String productChoice = "Product";
+    String product = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_specification_activiy);
+
         ImageView imageview = (ImageView) findViewById(R.id.logo);
         imageview.setImageAlpha(100);
+
+        Intent intent = getIntent();
+        TextView textview = (TextView) findViewById(R.id.Choice);
+        product = intent.getStringExtra(productChoice) + "\n";
+        textview.setText(product);
     }
 
     public void goBack(View view){
@@ -23,8 +36,42 @@ public class Product_Specification_Activity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void configureOrder(View view){
-        Intent intent= new Intent(this, Product_Ordering_Activity.class);
+    public void addItem(View view){
+
+        RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radioGroupRolls);
+        int id = radioGroup.getCheckedRadioButtonId();
+        String choice = "";
+        String toggleButtonOption = "";
+        String toSendBack = " ";
+
+        RadioButton radioButton = findViewById(id);
+        switch (id){
+
+            case R.id.chocSyrup:
+                choice = radioButton.getText().toString();
+                break;
+            case R.id.caramelSyrup:
+                choice = radioButton.getText().toString();
+                break;
+            case R.id.StrawberrySyrup:
+                choice = radioButton.getText().toString();
+                break;
+        }
+
+        ToggleButton toggleButton = (ToggleButton) findViewById(R.id.amount);
+        boolean toggle = toggleButton.isChecked();
+
+        if(toggle){
+            toggleButtonOption = toggleButton.getText().toString();
+        }
+
+
+
+        Intent intent = new Intent(this, Product_Ordering_Activity.class);
+        toSendBack = product + " " + choice + "\n" + toggleButtonOption;
+        intent.putExtra(Product_Ordering_Activity.specifications,toSendBack);
         startActivity(intent);
+
+
     }
 }
